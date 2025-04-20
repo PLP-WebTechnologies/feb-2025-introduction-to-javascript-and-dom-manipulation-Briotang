8b181c8b080ae8fd3d974371e6fac56d2ea3858d
Briotang <Tangpeeep@gmail.com>
2025-04-20 17:12:10 +0300

    assignment submission

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello Javascript!";
+  }
+
+
+function myFunc() {
+    document.getElementById("end").style.fontSize="35px";
+  }
+
+function myFunct() {
+    document.getElementById("new").style.display="none"
+
+  }
